f3977e617d32fbfe967ecaa0e43d5675eeadd117
CNwenwen <1218858474@qq.com>
2024-11-16 07:27:15 +0800

    feat:初始代码提交

---
+++ softkeyboard/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace softkeyboard
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nindex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern UInt32 GetWindowLong(IntPtr hWnd, int index);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowInteropHelper windowInteropHelper = new WindowInteropHelper(this);
+
+            IntPtr intPtr = windowInteropHelper.Handle;
+
+            int value = -20;
+
+            SetWindowLong(intPtr, -20, 0x8000000);
+        }
+
+        private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.DragMove();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsBtn_click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow window = new();
+            window.ShowDialog();
+        }
+    }
+}
---
+++ softkeyboard/SettingsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iNKORE.UI.WPF.Modern.Controls;
+
+
+namespace softkeyboard;
+
+public partial class SettingsWindow : Window
+{
+    public bool StartOnStartup { get; set; }
+    public class Settings
+    {
+        public bool startonstart { get; set; }
+    }
+    public SettingsWindow()
+    {
+        InitializeComponent();
+        if (File.Exists(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Settings\\Settings.json"))
+        {
+            Settings settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Settings\\Settings.json"));
+            StartOnStartupToggleSwitch.IsOn = settings.startonstart;
+        }
+    }
+
+    private async void StartOnStartup_ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
+    {
+        Settings settings = new();
+        var senderToggleSwitch = sender as ToggleSwitch;
+        settings.startonstart = senderToggleSwitch.IsOn;
+        Directory.CreateDirectory(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Settings\\");
+        string fileName = "Settings\\Settings.json";
+        await using FileStream createStream = File.Create(fileName);
+        await System.Text.Json.JsonSerializer.SerializeAsync(createStream, settings);
+    }
+}
